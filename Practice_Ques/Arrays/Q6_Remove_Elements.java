4a4fbff80c8d86b222bf355583ec780f398ee8e7
Ritesh Awadhiya <riteshawadhiya2004@gmail.com>
2026-05-31 11:13:29 +0530

    removeElme

---
+++ Practice_Ques/Arrays/Q6_Remove_Elements.java
@@ -0,0 +1,27 @@
+package Practice_Ques.Arrays;
+
+public class Q6_Remove_Elements {
+    public static void main(String[] args) {
+        int [] arr = new int[]{0,1,2,2,3,0,4,2};
+        int val = 2;
+        int ans = removeElme(arr, val);
+        System.out.println("Number of elements remaining after removing " + val + " : " + ans);
+
+        System.out.print("Array after removal: ");
+        for (int i = 0; i < ans; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static int removeElme(int[]arr , int val){
+        int n = arr.length;
+        int k = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] != val){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
